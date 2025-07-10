7926c7dc75bc142d08f68975e8b7ce0b2ef18efc
Danilo Takeo <danilot.kanizawa@gmail.com>
2025-07-10 10:08:45 -0300

    Criação da classe BaseIT que servirá de base para as classes de testes de integração com Testcontainers

---
+++ mongodb/src/test/java/com/bustation/mongodb/integration_tests/BaseIT.java
@@ -0,0 +1,28 @@
+package com.bustation.mongodb.integration_tests;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+@SpringBootTest
+@Testcontainers
+public abstract class BaseIT {
+
+    @Container
+    static MongoDBContainer mongoContainer = new MongoDBContainer("mongo:6.0");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoContainer::getReplicaSetUrl);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        mongoContainer.start();
+    }
+}
